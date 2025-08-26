aa854b92529fc6f52eec6d01955a15210e3d7eab
habaanis <zenanis75@hotmail.com>
2025-08-26 17:55:07 +0200

    feat: Ajout de l'API Google Maps

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+// Utilisez cette apiKey pour initialiser la carte
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
