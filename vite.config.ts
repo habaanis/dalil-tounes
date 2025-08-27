cbeb13f41d8d0b8807837583c63261a566eeebab
habaanis <zenanis75@hotmail.com>
2025-08-27 16:58:58 +0200

    fix: Finalisation de la configuration Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './',
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     minify: 'terser',
